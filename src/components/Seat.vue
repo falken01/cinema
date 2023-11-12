b922b759ce5754d7b1088613c2e3757fdc38af55
michal hutnik <michhut911@student.polsl.pl>
2023-11-12 12:42:27 +0100

    sal

---
+++ src/components/Seat.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="seat mt-4 mr-2" :class="{reserved : isReserved, free: isFree}" @click="take">
+      {{ num }}
+    </div>
+  </template>
+  
+  <script>
+
+  export default {
+    name: "cinemaSeat",
+    props: {
+      num: Number,
+    },
+    data () {
+      return {
+        isReserved:false,
+        isFree : true
+      }
+    },
+    methods:{
+      take(){
+        this.isFree = !this.isFree
+        this.isReserved = !this.isReserved;
+      }
+    }
+  };
+  </script>
+  
+  <style scoped lang="scss">
+  .seat {
+    width: 25px;
+    height: 25px;
+    color: white
+  }
+  .free {
+    background-color: green;
+  }
+
+  .taken {
+    background-color: rgb(255, 0, 0) !important;
+  }
+  .reserved {
+    background-color: black;
+  }
+  </style>
+  
---
+++ src/views/CinemaView.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="cinema mt-16">
+        <div v-for="j in 4" :num="j" :key="j" class="d-flex justify-center">
+            <seat v-for="i in 10" :num="i" :key="i" @click="$emit('toggle')"/>
+        </div>
+    </div>
+  </template>
+  
+  <script>
+  // @ is an alias to /src
+  import seat from '../components/Seat.vue'
+
+  export default {
+    name: "CinemaView",
+    components: {
+        seat
+    },
+    data() {
+      return {
+        
+      }
+    }
+  };
+  </script>
+  <style>
+  .cinema {
+    margin: 0 auto;
+  }
+  </style>
