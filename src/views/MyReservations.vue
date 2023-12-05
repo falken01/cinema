8cb0d88e8a91e7ebcb0f5466e1dd2016e67f9af6
Artur Markiel <artumar972@student.polsl.pl>
2023-12-05 20:17:18 +0100

    Filtrowanie w myReservations id!=0

--- src/views/MyReservations.vue
+++ src/views/MyReservations.vue
@@ -27,44 +27,46 @@ export default {
     if (res && res.status == 200) {
       for (let reservation of res.data) {
         console.log(reservation);
-        let showing_res = await this.$store.dispatch(
-          "showing/getShowing",
-          reservation.showingId
-        );
-        console.log(showing_res.data);
-
-        if (showing_res && showing_res.status == 200) {
-          let movie_id = showing_res.data.movieId;
-          let hall_id = showing_res.data.hallId;
-
-          let movie_res = await this.$store.dispatch(
-            "movie/getMovie",
-            movie_id
+        if (reservation.reservationId != 0) {
+          let showing_res = await this.$store.dispatch(
+            "showing/getShowing",
+            reservation.showingId
           );
-          let hall_res = await this.$store.dispatch("hall/getHall", hall_id);
-          console.log("---");
-          console.log(hall_res);
-          console.log(movie_res);
+          console.log(showing_res.data);
 
-          if (
-            movie_res &&
-            movie_res.status == 200 &&
-            hall_res &&
-            hall_res.status == 200
-          ) {
-            this.ReservationsText =
-              this.ReservationsText +
-              "Rezarwacja nr " +
-              reservation.reservationId +
-              " Film: " +
-              movie_res.data.title +
-              " Sala: " +
-              hall_res.data.name +
-              " Rząd: " +
-              reservation.row +
-              " Miejsce: " +
-              reservation.column +
-              "\n\n";
+          if (showing_res && showing_res.status == 200) {
+            let movie_id = showing_res.data.movieId;
+            let hall_id = showing_res.data.hallId;
+
+            let movie_res = await this.$store.dispatch(
+              "movie/getMovie",
+              movie_id
+            );
+            let hall_res = await this.$store.dispatch("hall/getHall", hall_id);
+            console.log("---");
+            console.log(hall_res);
+            console.log(movie_res);
+
+            if (
+              movie_res &&
+              movie_res.status == 200 &&
+              hall_res &&
+              hall_res.status == 200
+            ) {
+              this.ReservationsText =
+                this.ReservationsText +
+                "Rezarwacja nr " +
+                reservation.reservationId +
+                " Film: " +
+                movie_res.data.title +
+                " Sala: " +
+                hall_res.data.name +
+                " Rząd: " +
+                reservation.row +
+                " Miejsce: " +
+                reservation.column +
+                "\n\n";
+            }
           }
         }
       }
